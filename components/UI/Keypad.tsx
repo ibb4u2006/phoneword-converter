7eb25bdf7942e3eadb5290b84aa532277d27e36e
Ibrahim Bello <ibb@Ibrahims-MacBook-Pro.local>
2022-05-09 01:16:30 +0200

    Toggle keypad

--- components/UI/Keypad.tsx
+++ components/UI/Keypad.tsx
@@ -6,6 +6,7 @@ import {
   Text,
   ToastRoot,
 } from "@kiwicom/orbit-components";
+import { ChevronDown, ChevronUp } from "@kiwicom/orbit-components/lib/icons";
 import { KEYPAD } from "data/keypad";
 import * as React from "react";
 
@@ -14,31 +15,40 @@ interface IKeypadProps {
 }
 
 const Keypad: React.FunctionComponent<IKeypadProps> = ({ handleClick }) => {
+  const [toggle, setToggle] = React.useState(true);
   return (
     <div className="keypad-section">
       <Box display="flex" direction="row" justify="between" align="center">
         <Heading as="h2">T9 Keypad</Heading>
+        <Button
+          iconRight={toggle ? <ChevronUp /> : <ChevronDown />}
+          onClick={() => setToggle(!toggle)}
+        >
+          {toggle ? "Hide" : "Show"} T9 Keypad
+        </Button>
       </Box>
-      <div className="keypad">
-        <Grid columns="repeat(3, 1fr)" rows="repeat(4, 1fr)" gap="10px">
-          {KEYPAD.map((key) => {
-            return (
-              <React.Fragment key={key.id}>
-                <ToastRoot dismissTimeout={2000} placement="top-center" />
-                <Button
-                  onClick={() => {
-                    const number = key.number;
-                    handleClick(key.number.toString());
-                  }}
-                >
-                  <Heading as="h3">{key.number}</Heading>
-                  <Text as="p">{key.letters}</Text>
-                </Button>
-              </React.Fragment>
-            );
-          })}
-        </Grid>
-      </div>
+      {toggle && (
+        <div className="keypad">
+          <Grid columns="repeat(3, 1fr)" rows="repeat(4, 1fr)" gap="10px">
+            {KEYPAD.map((key) => {
+              return (
+                <React.Fragment key={key.id}>
+                  <ToastRoot dismissTimeout={2000} placement="top-center" />
+                  <Button
+                    onClick={() => {
+                      const number = key.number;
+                      handleClick(key.number.toString());
+                    }}
+                  >
+                    <Heading as="h3">{key.number}</Heading>
+                    <Text as="p">{key.letters}</Text>
+                  </Button>
+                </React.Fragment>
+              );
+            })}
+          </Grid>
+        </div>
+      )}
     </div>
   );
 };

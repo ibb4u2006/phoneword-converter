cda36eb73786404cf7c9ca3bbae9f4e1e6a8b604
Ibrahim Bello <ibb@Ibrahims-MacBook-Pro.local>
2022-05-09 02:57:29 +0200

    Copyright

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 interface IFooterProps {}
 
 const Footer: React.FunctionComponent<IFooterProps> = (props) => {
-  return <footer>Footer Component</footer>;
+  return <footer>Phoneword Converter App</footer>;
 };
 
 export default Footer;
